c9418695c128535dcb5d8e3981b34be6a95b53d1
Bharat Kharbanda <52108074+ravencode@users.noreply.github.com>
2020-08-26 16:43:32 +0530

    Update Pattern 1.java

--- Basics of Programming/Patterns/Pattern 1.java
+++ Basics of Programming/Patterns/Pattern 1.java
@@ -8,9 +8,9 @@ public class Main {
         int n = scn.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("*\t");
             }
-            System.out.println("");
+            System.out.println();
 
         }
     }
