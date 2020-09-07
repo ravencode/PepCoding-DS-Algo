5aade248434a0f9bcbe135a26d31f57b9d9ba030
Bharat Kharbanda <52108074+ravencode@users.noreply.github.com>
2020-09-07 13:26:52 +0530

    Create Pattern 19.java

---
+++ Basics of Programming/Patterns/Pattern 19.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+     // write ur code here
+    int n = scn.nextInt();
+    int str = n;
+    int spc = 1;
+    for(int i = 1; i <= n; i++){
+      if(i ==1){
+          if(j == 
+      }else if(i <= n /2){
+          
+      }else if(i == n / 2 + 1){
+          
+      }else if(i < n){
+          
+      }else{
+          
+      }
+        System.out.println();
+    }
+ }
+}
