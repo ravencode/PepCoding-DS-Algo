0b21cc4b84c0719a53614f444352e7563baa3136
Hardik Sharma <44233098+MPL0Y@users.noreply.github.com>
2021-10-03 22:02:27 +0530

    Removed egyptian brackets
    They are bad

--- Basics of Programming/2D-Arrays/2D-Arrays demo.java
+++ Basics of Programming/2D-Arrays/2D-Arrays demo.java
@@ -1,22 +1,27 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) throws Exception {
-        // write your code here
+    public static void main(String[] args) throws Exception
+    {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int m = scn.nextInt();
         int[][] arr = new int[n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
                 arr[i][j] = scn.nextInt();
             }
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
